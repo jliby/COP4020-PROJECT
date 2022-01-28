5ac56ca6ff26c003fc37d7efeedfbe9184ab4171
James <35617601+jliby@users.noreply.github.com>
2022-01-28 00:00:15 -0500

    Added Lexer Class invocation to LexerTest

--- src/edu/ufl/cise/plc/ILexer.java
+++ src/edu/ufl/cise/plc/ILexer.java
@@ -1,13 +1,9 @@
 package edu.ufl.cise.plc;
 
-import org.junit.jupiter.api.Test;
-
 import java.util.ArrayList;
 import java.util.List;
 
 import static edu.ufl.cise.plc.IToken.Kind.EOF;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public interface ILexer {
 
@@ -16,12 +12,19 @@ public interface ILexer {
 	int line = 1;
 	String source = null;
 	List<IToken> tokens = new ArrayList<>();
+
 	IToken next() throws LexicalException;
+
 	IToken peek() throws LexicalException;
+
 	char advance();
+
 	void addToken(IToken.Kind type);
+
 	void addToken(IToken.Kind type, Object literal);
+
 	boolean isAtEnd();
+
 	void scanToken();
 
 	List<IToken.Token> Scanner(String source);
@@ -42,8 +45,8 @@ public interface ILexer {
 		@Override
 		public IToken next() throws LexicalException {
 
-	//		 if (current + 1 >= source.length()) return '\0';
-	//		 return source.charAt(current + 1);
+			//		 if (current + 1 >= source.length()) return '\0';
+			//		 return source.charAt(current + 1);
 
 			// convert into token and return
 			return null;
@@ -51,8 +54,8 @@ public interface ILexer {
 
 		@Override
 		public IToken peek() throws LexicalException {
-	//		 if (isAtEnd()) return '\0';
-	//		     return source.charAt(current);
+			//		 if (isAtEnd()) return '\0';
+			//		     return source.charAt(current);
 
 			// convert into token and return
 
@@ -79,17 +82,17 @@ public interface ILexer {
 			char c = advance();
 			switch (c) {
 
-	//		  example
-	//			  case '(': addToken(IToken.Kind.LEFT_PAREN); break;
-	//			  case ')': addToken(RIGHT_PAREN); break;
-	//			  case '{': addToken(LEFT_BRACE); break;
-	//			  case '}': addToken(RIGHT_BRACE); break;
-	//			  case ',': addToken(COMMA); break;
-	//			  case '.': addToken(DOT); break;
-	//			  case '-': addToken(MINUS); break;
-	//			  case '+': addToken(PLUS); break;
-	//			  case ';': addToken(SEMICOLON); break;
-	//			  case '*': addToken(STAR); break; // [slash]
+				//		  example
+				//			  case '(': addToken(IToken.Kind.LEFT_PAREN); break;
+				//			  case ')': addToken(RIGHT_PAREN); break;
+				//			  case '{': addToken(LEFT_BRACE); break;
+				//			  case '}': addToken(RIGHT_BRACE); break;
+				//			  case ',': addToken(COMMA); break;
+				//			  case '.': addToken(DOT); break;
+				//			  case '-': addToken(MINUS); break;
+				//			  case '+': addToken(PLUS); break;
+				//			  case ';': addToken(SEMICOLON); break;
+				//			  case '*': addToken(STAR); break; // [slash]
 			}
 		}
 
@@ -117,181 +120,6 @@ public interface ILexer {
 			tokens.add(new IToken.Token(type, text, literal, line));
 		}
 
-
-		public static class LexerTests {
-
-			ILexer getLexer(String input){
-				 return CompilerComponentFactory.getLexer(input);
-			}
-
-			//makes it easy to turn output on and off (and less typing than System.out.println)
-			static final boolean VERBOSE = true;
-			void show(Object obj) {
-				if(VERBOSE) {
-					System.out.println(obj);
-				}
-			}
-
-			//check that this token has the expected kind
-			void checkToken(IToken t, IToken.Kind expectedKind) {
-				assertEquals(expectedKind, t.getKind());
-			}
-
-			//check that the token has the expected kind and position
-			void checkToken(IToken t, IToken.Kind expectedKind, int expectedLine, int expectedColumn){
-				assertEquals(expectedKind, t.getKind());
-				assertEquals(new IToken.SourceLocation(expectedLine,expectedColumn), t.getSourceLocation());
-			}
-
-			//check that this token is an IDENT and has the expected name
-			void checkIdent(IToken t, String expectedName){
-				assertEquals(IToken.Kind.IDENT, t.getKind());
-				assertEquals(expectedName, t.getText());
-			}
-
-			//check that this token is an IDENT, has the expected name, and has the expected position
-			void checkIdent(IToken t, String expectedName, int expectedLine, int expectedColumn){
-				checkIdent(t,expectedName);
-				assertEquals(new IToken.SourceLocation(expectedLine,expectedColumn), t.getSourceLocation());
-			}
-
-			//check that this token is an INT_LIT with expected int value
-			void checkInt(IToken t, int expectedValue) {
-				assertEquals(IToken.Kind.INT_LIT, t.getKind());
-				assertEquals(expectedValue, t.getIntValue());
-			}
-
-			//check that this token  is an INT_LIT with expected int value and position
-			void checkInt(IToken t, int expectedValue, int expectedLine, int expectedColumn) {
-				checkInt(t,expectedValue);
-				assertEquals(new IToken.SourceLocation(expectedLine,expectedColumn), t.getSourceLocation());
-			}
-
-			//check that this token is the EOF token
-			void checkEOF(IToken t) {
-				checkToken(t, EOF);
-			}
-
-
-			//The lexer should add an EOF token to the end.
-			@Test
-			void testEmpty() throws LexicalException {
-				String input = "";
-				show(input);
-				ILexer lexer = getLexer(input);
-				checkEOF(lexer.next());
-			}
-
-			//A couple of single character tokens
-			@Test
-			void testSingleChar0() throws LexicalException {
-				String input = """
-						+ 
-						- 	 
-						""";
-				show(input);
-				ILexer lexer = getLexer(input);
-				checkToken(lexer.next(), IToken.Kind.PLUS, 0,0);
-				checkToken(lexer.next(), IToken.Kind.MINUS, 1,0);
-				checkEOF(lexer.next());
-			}
-
-			//comments should be skipped
-			@Test
-			void testComment0() throws LexicalException {
-				//Note that the quotes around "This is a string" are passed to the lexer.
-				String input = """
-						"This is a string"
-						#this is a comment
-						*
-						""";
-				show(input);
-				ILexer lexer = getLexer(input);
-				checkToken(lexer.next(), IToken.Kind.STRING_LIT, 0,0);
-				checkToken(lexer.next(), IToken.Kind.TIMES, 2,0);
-				checkEOF(lexer.next());
-			}
-
-			//Example for testing input with an illegal character
-			@Test
-			void testError0() throws LexicalException {
-				String input = """
-						abc
-						@
-						""";
-				show(input);
-				ILexer lexer = getLexer(input);
-				//this check should succeed
-				checkIdent(lexer.next(), "abc");
-				//this is expected to throw an exception since @ is not a legal
-				//character unless it is part of a string or comment
-				assertThrows(LexicalException.class, () -> {
-					@SuppressWarnings("unused")
-					IToken token = lexer.next();
-				});
-			}
-
-			//Several identifiers to test positions
-			@Test
-			public void testIdent0() throws LexicalException {
-				String input = """
-						abc
-						  def
-							 ghi
-		
-						""";
-				show(input);
-				ILexer lexer = getLexer(input);
-				checkIdent(lexer.next(), "abc", 0,0);
-				checkIdent(lexer.next(), "def", 1,2);
-				checkIdent(lexer.next(), "ghi", 2,5);
-				checkEOF(lexer.next());
-			}
-
-
-			@Test
-			public void testEquals0() throws LexicalException {
-				String input = """
-						= == ===
-						""";
-				show(input);
-				ILexer lexer = getLexer(input);
-				checkToken(lexer.next(), IToken.Kind.ASSIGN,0,0);
-				checkToken(lexer.next(), IToken.Kind.EQUALS,0,2);
-				checkToken(lexer.next(), IToken.Kind.EQUALS,0,5);
-				checkToken(lexer.next(), IToken.Kind.ASSIGN,0,7);
-				checkEOF(lexer.next());
-			}
-
-			@Test
-			public void testIdenInt() throws LexicalException {
-				String input = """
-						a123 456b
-						""";
-				show(input);
-				ILexer lexer = getLexer(input);
-				checkIdent(lexer.next(), "a123", 0,0);
-				checkInt(lexer.next(), 456, 0,5);
-				checkIdent(lexer.next(), "b",0,8);
-				checkEOF(lexer.next());
-				}
-
-
-			//example showing how to handle number that are too big.
-			@Test
-			public void testIntTooBig() throws LexicalException {
-				String input = """
-						42
-						99999999999999999999999999999999999999999999999999999999999999999999999
-						""";
-				ILexer lexer = getLexer(input);
-				checkInt(lexer.next(),42);
-				Exception e = assertThrows(LexicalException.class, () -> {
-					lexer.next();
-				});
-			}
-
-		}
 	}
 }
 
