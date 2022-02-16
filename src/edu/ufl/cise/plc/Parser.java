aeea9306090e3e2a55bce7a2b0cd9db0d44cc132
j-jurado <73544267+j-jurado@users.noreply.github.com>
2022-02-16 15:33:44 -0500

    Cleanup

--- src/edu/ufl/cise/plc/Parser.java
+++ src/edu/ufl/cise/plc/Parser.java
@@ -1,15 +1,11 @@
 package edu.ufl.cise.plc;
-
 import edu.ufl.cise.plc.ast.*;
-
-import java.beans.Expression;
 import java.util.List;
-
 import static edu.ufl.cise.plc.IToken.Kind.*;
 
 public class Parser implements IParser {
 
-// tokens list
+    // tokens list
     private final List<Token> tokens;
     private int current = 0;
     public Token currentToken;
@@ -27,6 +23,8 @@ public class Parser implements IParser {
         if (isAtEnd()) return false;
         return peek().type == type;
     }
+    
+    // Consume advances to the next token
     private Token consume() {
         if (!isAtEnd()){
             current++;
@@ -34,7 +32,6 @@ public class Parser implements IParser {
         }
         return previous();
     }
-    // Consume advances to the next token
 
     private boolean isAtEnd() {
         return peek().type == EOF;
@@ -47,7 +44,6 @@ public class Parser implements IParser {
     private Token previous() {
         return tokens.get(current - 1);
     }
-
 
     protected boolean isKind(Token.Kind kind) {
         return currentToken.type == kind;
@@ -69,118 +65,16 @@ public class Parser implements IParser {
                 return true;
             }
         }
-
         return false;
     }
+
     @Override
     public ASTNode parse() throws PLCException {
-        //ASTNode AST;
-//        try {
-//         AST = expr();
-//        }
-//        catch (Exception e) {
-//            return null;
-//        }
-        //AST = expr();
-        AST = exprNew();
+        AST = expr();
         return AST;
     }
-
+    
     public Expr expr() throws SyntaxException, LexicalException {
-        Token firstToken = currentToken;
-        Expr left = null;
-        Expr right = null;
-        left = term();
-        while (isKind(PLUS) || isKind(MINUS)){
-            IToken op = currentToken;
-            firstToken = consume();
-            right = term();
-            left = new BinaryExpr(firstToken, left, op, right);
-        }
-        return left;
-
-//        if ( isKind(KW_IF)) {
-//            ConditionalExpression();
-//        } else if (isKind(BANG, MINUS, COLOR_OP, IMAGE_OP)) {
-//            LogicalOrExpression();
-//        }
-
-    }
-    public Expr term() throws SyntaxException, LexicalException {
-        Token firstToken = currentToken;
-        Expr left = null;
-        Expr right = null;
-        left = factor();
-        while(isKind(TIMES) || isKind(DIV)){
-            IToken op = currentToken;
-            firstToken = consume();
-            right = factor();
-            left = new BinaryExpr(firstToken, left, op, right);
-        }
-        return left;
-    }
-
-    public Expr factor() throws SyntaxException, LexicalException {
-        IToken firstToken = currentToken;
-        Expr e = null;
-        if(isKind(INT_LIT)){
-            e = new IntLitExpr(firstToken);
-            consume();
-        }
-        else if (isKind(LPAREN)){
-            consume();
-            e = expr();
-            match(RPAREN);
-        }
-        else if (isKind(BOOLEAN_LIT)){
-            e = new BooleanLitExpr(firstToken);
-            consume();
-        }
-        else if (isKind(STRING_LIT)){
-            e = new StringLitExpr(firstToken);
-            consume();
-        }
-        else if (isKind(FLOAT_LIT)){
-            e = new FloatLitExpr(firstToken);
-            consume();
-        }
-        else if (isKind(IDENT)){
-            e = new IdentExpr(firstToken);
-            consume();
-        }
-        else if (isKind(KW_IF)){
-            IToken current = firstToken;
-            consume();
-            match(LPAREN);
-            Expr condition = expr();
-            match(RPAREN);
-            Expr trueCase = expr();
-            match(KW_ELSE);
-            e = new ConditionalExpr(current, condition, trueCase, expr());
-            match(KW_FI);
-        }
-        else if (isKind(BANG)|| isKind(MINUS) || isKind(COLOR_OP) || isKind(IMAGE_OP))
-        {
-            IToken op = firstToken;
-            consume();
-            Expr right = expr();
-            e = new UnaryExpr(op, op, right);
-        }
-        else {
-            if (isKind(ERROR)){
-                throw new LexicalException("");
-            }
-            else{
-                throw new SyntaxException("");
-            }
-        }
-        return e;
-    }
-
-
-    /*=== Restructure ===*/
-
-    public Expr exprNew() throws SyntaxException, LexicalException {
         Token firstToken = currentToken;
         Expr left = null;
         Expr right = null;
@@ -193,14 +87,15 @@ public class Parser implements IParser {
         }
         return left;
     }
+
     public Expr conditionExpr() throws SyntaxException, LexicalException{
         Token firstToken = currentToken;
         Expr e = null;
         consume();
         match(LPAREN);
-        Expr condition = exprNew();
+        Expr condition = expr();
         match(RPAREN);
-        Expr trueCase = exprNew();
+        Expr trueCase = expr();
         match(KW_ELSE);
         e = new ConditionalExpr(firstToken, condition, trueCase, expr());
         if(!match(KW_FI)){
@@ -208,6 +103,7 @@ public class Parser implements IParser {
         }
         return e;
     }
+
     public Expr logicalOrExpr() throws SyntaxException, LexicalException{
         Token firstToken = currentToken;
         Expr left = null;
@@ -221,6 +117,7 @@ public class Parser implements IParser {
         }
         return left;
     }
+
     public Expr logicalAndExpr() throws SyntaxException, LexicalException{
         Token firstToken = currentToken;
         Expr left = null;
@@ -234,6 +131,7 @@ public class Parser implements IParser {
         }
         return left;
     }
+
     public Expr comparisonExpr() throws SyntaxException, LexicalException{
         Token firstToken = currentToken;
         Expr left = null;
@@ -247,6 +145,7 @@ public class Parser implements IParser {
         }
         return left;
     }
+
     public Expr additiveExpr() throws SyntaxException, LexicalException{
         Token firstToken = currentToken;
         Expr left = null;
@@ -260,6 +159,7 @@ public class Parser implements IParser {
         }
         return left;
     }
+
     public Expr multiplicativeExpr() throws SyntaxException, LexicalException{
         Token firstToken = currentToken;
         Expr left = null;
@@ -273,13 +173,14 @@ public class Parser implements IParser {
         }
         return left;
     }
+
     public Expr UnaryExpr() throws SyntaxException, LexicalException{
         Token firstToken = currentToken;
         Expr e = null;
         if (isKind(BANG) || isKind(MINUS) || isKind(COLOR_OP) || isKind(IMAGE_OP)){
             Token op = currentToken;
             consume();
-            Expr unaryExpr = exprNew();
+            Expr unaryExpr = expr();
             e = new UnaryExpr(firstToken, op, unaryExpr);
         }
         else{
@@ -287,6 +188,7 @@ public class Parser implements IParser {
         }
         return e;
     }
+
     public Expr UnaryExprPostfix() throws SyntaxException, LexicalException{
         Token firstToken = currentToken;
         Expr e = null;
@@ -310,7 +212,7 @@ public class Parser implements IParser {
             consume();
         }
         else if (isKind(LPAREN)){
-            e = exprNew();
+            e = expr();
             match(RPAREN);
         }
         else if (isKind(ERROR)){
@@ -321,108 +223,13 @@ public class Parser implements IParser {
         }
         if(isKind(LSQUARE)){
             consume();
-            Expr e1 = exprNew();
+            Expr e1 = expr();
             match(COMMA);
-            Expr e2 = exprNew();
+            Expr e2 = expr();
             match(RSQUARE);
             PixelSelector pixelSel = new PixelSelector(firstToken, e1, e2);
             e = new UnaryExprPostfix(firstToken, e, pixelSel);
         }
         return e;
     }
-
-
-//    public void ConditionalExpression() {
-//        if (isKind(KW_IF)) {
-//            consume();
-//        } else {
-//            // return an error
-//        }
-//        if (isKind(LPAREN)) {
-//            consume();
-//            //
-//        } else {
-//            // return an err
-//        }
-//        Expression();
-//        if (isKind(RPAREN)) {
-//            consume();
-//        }
-//
-//        Expression();
-//
-//        if (isKind(KW_ELSE)) {
-//            consume();
-//        }
-//        else {
-//            // return an error
-//        }
-//
-//        Expression();
-//
-//        if(isKind(KW_FI)) {
-//            consume();
-//        }
-//
-//        return;
-//
-//
-//    }
-//
-//    public Expr LogicalOrExpression() {
-//        LogicalAndExpression();
-//        if(isKind(OR)) {
-//            while (isKind(OR)) {
-//                consume();
-//                LogicalAndExpression();
-//            }
-//        }
-//    }
-//
-////
-////    ComparisonExpr ::=
-////    AdditiveExpr ( ('<' | '>' | '==' | '!=' | '<=' | '>=') AdditiveExpr)*
-//
-//    public Expr ComparisonExpression() {
-//
-//        AdditiveExpression();
-//        if(isKind(LANGLE, RANGLE, EQUALS, NOT_EQUALS, GE, LE)){
-//            while(isKind(LANGLE, RANGLE, EQUALS, NOT_EQUALS, GE, LE)) {
-//                consume();
-//                AdditiveExpression();
-//            }
-//        } else {
-//            // err
-//            return  null;
-//        }
-//        return null;
-//    }
-//
-//     public Expr LogicalAndExpression() {
-//        ComparisonExpression();
-//         if(isKind(AND)) {
-//             while (isKind(AND)) {
-//                 consume();
-//                 ComparisonExpression();
-//             }
-//         } else {
-//
-//         }
-//
-//     }
-//    public Expr AdditiveExpression() {
-//
-//        }
-//    public Expr MultiplicativeExpression() {
-//
-//        }
-//    public Expr UnaryExpression() {
-//
-//        }
-//    public Expr PrimaryExpression() {
-//
-//        }
-//    public Expr PixelSelector() {
-//
-//        }
 }
