a33fda57adc798fb810c161af513414ada7ac770
j-jurado <73544267+j-jurado@users.noreply.github.com>
2022-03-22 16:25:18 -0400

    Updated CompileComponentFactory

--- src/edu/ufl/cise/plc/CompilerComponentFactory.java
+++ src/edu/ufl/cise/plc/CompilerComponentFactory.java
@@ -1,24 +1,20 @@
 package edu.ufl.cise.plc;
-
-//This class eliminates hard coded dependencies on the actual Lexer class.  You can call your lexer whatever you
-//want as long as it implements the ILexer interface and you have provided an appropriate body for the getLexer method.
-
+import edu.ufl.cise.plc.ast.ASTVisitor;
 
 public class CompilerComponentFactory {
-
-	//This method will be invoked to get an instance of your lexer.
+	
 	public static Lexer getLexer(String input) {
-		//TODO:  modify this method so it returns an instance of your Lexer instead of throwing the exception.
-		//for example:
-		//return new Lexer(input);
 		Lexer test = new Lexer(input);
 		test.Scanner();
 		return test;
-//		throw new UnsupportedOperationException("CompilerComponentFactory must be modified to return an instance of your lexer");
 	}
 
 	public static IParser getParser(String input) {
 		Lexer lexer = getLexer(input);
 		return new Parser(lexer.getTokens());
+	}
+
+	public static ASTVisitor getTypeChecker(){
+		return new TypeCheckVisitor();
 	}
 }
